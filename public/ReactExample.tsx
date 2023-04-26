15d07dec15b00cd5317543e6366a143f8a4ca369
Marko Vujanic <marko.vujanic@axa.ch>
2023-04-26 14:33:25 +0200

    format: gorko

--- public/ReactExample.tsx
+++ public/ReactExample.tsx
@@ -1,10 +1,10 @@
-/* @eslint-disable */
+// @ts-nocheck - We don't care about react here, just for editor testing
 function Example() {
   return (
     <div>
       <h1 className="text-3"></h1>
       <p className="measure-long lg:hidden-yes">Fido</p>
-      <p className=""></p>
+      <p className="box-block"></p>
     </div>
   );
 }
